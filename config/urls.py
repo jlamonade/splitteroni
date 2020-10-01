d1a2dc2903d2135dca9a21bcd80db6fb065e6c4d
jason <nyjasonlam@gmail.com>
2020-09-30 23:43:23 -0400

    Added login and signup

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+
+    # User management
+    path('users/', include('django.contrib.auth.urls')),
+
+    # Local apps
+    path('users/', include('users.urls')),
     path('', include('pages.urls')),
 ]
