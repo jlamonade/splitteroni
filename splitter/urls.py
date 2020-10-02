126d3b9a5b284ac3a391a0c07263728af80e7187
jason <nyjasonlam@gmail.com>
2020-10-02 12:33:15 -0400

    added create/delete person and person list in billdetail

--- splitter/urls.py
+++ splitter/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import BillCreateView, BillDetailView
+from .views import BillCreateView, BillDetailView, PersonCreateView, PersonDeleteView
 
 
 urlpatterns = [
     path('new/', BillCreateView.as_view(), name='bill-create'),
     path('<int:pk>/', BillDetailView.as_view(), name='bill-detail'),
+    path('<int:pk>/add-person/', PersonCreateView.as_view(), name='person-create'),
+    path('person/<int:pk>/delete/', PersonDeleteView.as_view(), name='person-delete'),
 ]
--- splitter/views.py
+++ splitter/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, DeleteView
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
 
 from .models import Bill, Person, Item
 
@@ -15,3 +17,31 @@ class BillDetailView(DetailView):
     model = Bill
     template_name = 'splitter/bill_detail.html'
     context_object_name = 'bill'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['people'] = Person.objects.filter(
+            bill=get_object_or_404(Bill, id=self.kwargs['pk']))
+        return context
+
+
+class PersonCreateView(CreateView):
+    model = Person
+    template_name = 'splitter/person_create.html'
+    fields = ('name',)
+
+    def form_valid(self, form):
+        self.bill = get_object_or_404(Bill, id=self.kwargs['pk'])
+        form.instance.bill = self.bill
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('bill-detail', args=[self.bill.id])
+
+
+class PersonDeleteView(DeleteView):
+    model = Person
+    template_name = 'splitter/person_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('bill-detail', args=[self.object.bill.id])
