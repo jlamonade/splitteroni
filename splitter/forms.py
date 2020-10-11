5043e4356adaa0a43f5b6f3c1d24dce83599dcf7
Jason <nyjasonlam@gmail.com>
2020-10-11 02:06:22 -0400

    added custom form labels, help_texts, and error messages

--- splitter/forms.py
+++ splitter/forms.py
@@ -1,4 +1,5 @@
 from django.forms import forms, ModelForm
+from django.utils.translation import gettext_lazy as _
 
 from .models import Bill
 
@@ -7,3 +8,23 @@ class BillCreateForm(ModelForm):
     class Meta:
         model = Bill
         fields = ('title', 'tax', 'tip',)
+        labels = {
+            'title': _('Name'),
+        }
+        help_texts = {
+            'title': _('The current date and time will be used if no name is provided'),
+            'tax': _('Please enter a currency amount. You can adjust this later.'),
+            'tip': _('Please enter a currency amount. You can adjust this later.'),
+        }
+        error_messages = {
+            'title': {
+                'max_length': _("Name is too long."),
+            },
+            'tax': {
+                'max_digits': _("Amount is too large."),
+            },
+            'title': {
+                'max_digits': _("Amount is too large."),
+            },
+        }
+
