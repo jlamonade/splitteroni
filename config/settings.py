5b04c8a10a2368f0a23156f2a47e2ede242b8726
jason <nyjasonlam@gmail.com>
2020-10-27 16:02:58 -0400

    Removed placeholder secret key

--- config/settings.py
+++ config/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY",
-                 default='ibgjsfdugqdpsp&hp9v1-zd6x$1=t2b8@-axz7c%vwl23io44$')
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
